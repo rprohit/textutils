bc5de6fc2be6d04b65c578aac6a9afd3c210a290
Rohit <rohit.pant.788@gmail.com>
2022-06-04 06:28:16 +0530

    Lec-6 : Default vs Named Exports .

---
+++ src/components/module.mjs
@@ -0,0 +1,12 @@
+const a = 'Harry';
+const b = 'Rohan';
+const c = 'Aakash';
+const d = 'Lovish';
+const e = 'Priyanka';
+
+
+
+export default e;
+export {a};
+export {b};
+export {c};
---
+++ src/components/module2.mjs
@@ -0,0 +1,6 @@
+import xyz, { a, b, c } from './module.mjs'
+
+console.log(xyz)
+console.log(a)
+console.log(c)
+console.log(b)
